e0e1e96b339c6b5d4aebae63ebd92cb1ad040685
UmerUsman <umerusman24605@gmail.com>
2024-11-24 14:19:54 +0500

    HELLO WORLD

--- main.cpp
+++ main.cpp
@@ -4,23 +4,7 @@ using namespace std;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Cyan);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+    printf("hello Wolrd");
 
     return 0;
 }
