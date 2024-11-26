1af144fa81891dff2721e9c4473b9b99f08ab7ab
UmerUsman <umerusman24605@gmail.com>
2024-11-26 21:22:05 +0500

    "Sfml Window Work"

---
+++ Hangman.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <cmath>
+#include <istream>
+#include <ostream>
+#include <string>
+#include "SFML/Graphics.hpp"
+#include "SFML/Audio.hpp"
+
+
+using namespace std;
+
+class Hangman {
+
+private:
+
+	string easyWords[100] = { "Hello" };
+	string mediumWords[100] = {};
+	string hardWords[100] = {};
+
+
+
+};
--- main.cpp
+++ main.cpp
@@ -1,28 +1,80 @@
 #include <SFML/Graphics.hpp>
-
-//#include "C:\Users\DELL\OneDrive\Desktop\OOP PROJECT\Final Project GameBOY\Snake.h"
 #include <iostream>
+#include "Hangman.h"
+
 using namespace std;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Red);
+ 
+    sf::RenderWindow window;
+    window.create(sf::VideoMode(1280, 720), "Hangman"); //VideoMode Size of window
+    //window.setFramerateLimit(60);
+   // we have to create a loop
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
+    sf::RectangleShape rect;
+
+    sf::Vector2f rectanglePosition(600, 350);
+    rect.setPosition(rectanglePosition);
+    rect.setSize(sf::Vector2f(100, 100)); //f = float, i = integer, u = unsigned int
+    rect.setFillColor(sf::Color::Red);
+
+    //sfml uses shapes top as (0,0)
+
+    float xVelocity = 3;
+    float yVelocity = 3;
+
+    sf::Event event;
+    //will hold info about an event
+
+    while (window.isOpen()) {
+
+       
+
+        while (window.pollEvent(event)) //different events ko manage karnay ke liye takay unk clash na aye event union?
         {
-            if (event.type == sf::Event::Closed)
+
+            switch (event.type)
+            {
+
+            case sf::Event::Closed:
+
                 window.close();
+                break;
+
+            case sf::Event::KeyPressed:
+
+                if (event.key.code == sf::Keyboard::Escape) {
+
+                    window.close();
+
+                }
+
+                break;
+
+            }
+
+            if (rectanglePosition.x < 0 || rectanglePosition.x > 1280 - 100) xVelocity *= -1; //negative wapis jaye
+            if (rectanglePosition.y < 0 || rectanglePosition.y > 720 - 100) yVelocity *= -1;
+
+            rectanglePosition.x += xVelocity;
+            rectanglePosition.y += yVelocity; //to move objects we have to add it to that position 
+            //still only doing in back end
+            rect.setPosition(rectanglePosition);
+
+            window.clear();
+            window.draw(rect);
+            window.display();
+
         }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
     }
+
+    //before drawing always be sure to clear the window otherwise you will drawon what ever their is left behind
+    //inside loop very important three funstions
+
+    std::cout << "Window creation" << " \n";
+    return 0;
 
     return 0;
 }
