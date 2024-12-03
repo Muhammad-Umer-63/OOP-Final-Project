3f78fcae544693d4783adc962560fd4ef91676e7
UmerUsman <umerusman24605@gmail.com>
2024-12-03 12:17:31 +0500

    Fonts

--- WordDictionary.h
+++ WordDictionary.h
@@ -19,8 +19,15 @@ private:
 	string* history;
 	string* total_history;
 
+	sf::Font font;
+	sf::Text letter_text;
+
 	void initVariables();
 	void populateArrays();
+	void initFont();
+	void initText();
+
+
 
 
 public:
@@ -28,19 +35,67 @@ public:
 	WordDictionary();
 	~WordDictionary();
 
-
+	void updateText();
+	void renderText();
 
 };
 
 void WordDictionary::initVariables() {
 
+	this->easy = new string[100];
+	this->medium = new string[100];
+	this->hard = new string[100];
+	this->history = NULL;
+	this->total_history = NULL;
 
+}
 
+void WordDictionary::populateArrays()
+{
+
+	
 
 
 }
 
+void WordDictionary::initFont()
+{
+
+	this->font.loadFromFile("");
+
+}
+
+void WordDictionary::initText()
+{
 
 
+}
+
+WordDictionary::WordDictionary() {
+	
+	this->initVariables();
+	this->populateArrays();
+
+}
+
+WordDictionary::~WordDictionary() {
+
+	delete[] easy; easy = NULL;
+	delete[] medium; medium = NULL;
+	delete[] hard; hard = NULL;
+	delete[] history; history = NULL;
+	delete[] total_history; total_history = NULL;
+
+}
+
+void WordDictionary::updateText()
+{
 
 
+}
+
+void WordDictionary::renderText()
+{
+
+
+}
